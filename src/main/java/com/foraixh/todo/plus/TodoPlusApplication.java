a60d2cfec8ab51ed66c7900a6b268bc7181fb392
foraixh <myvina@qq.com>
2021-03-16 22:10:22 +0800

    增加作者注释

--- src/main/java/com/foraixh/todo/plus/TodoPlusApplication.java
+++ src/main/java/com/foraixh/todo/plus/TodoPlusApplication.java
@@ -3,6 +3,11 @@ package com.foraixh.todo.plus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author myvina
+ * @date 2021/03/10 10:55
+ * @usage todolist服务实现类
+ */
 @SpringBootApplication
 public class TodoPlusApplication {
 
